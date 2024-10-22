fbc6bfac9fea088eb702553f08f9fe8a6c9cf3de
Artyom <art161994@yandex.ru>
2024-10-22 19:59:20 +0200

    add cookie and rise buttons to employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,12 +9,12 @@ import './app.css';
 function App() {
 
     const data = [
-            {name: 'Tony S.', salary: 1200, increase: false},
-            {name: 'Walter W.', salary: 900, increase: false},
-            {name: 'Robert O.', salary: 1500, increase: false},
-            {name: 'John W.', salary: 1900, increase: true},
-            {name: 'Dawid L.', salary: 1400, increase: false},
-            {name: 'Michael K.', salary: 800, increase: false},
+            {name: 'Tony S.', salary: 1200, increase: false, id:1},
+            {name: 'Walter W.', salary: 900, increase: false, id:2},
+            {name: 'Robert O.', salary: 1500, increase: false, id:3},
+            {name: 'John W.', salary: 1900, increase: true, id:4},
+            {name: 'Dawid L.', salary: 1400, increase: false, id:5},
+            {name: 'Michael K.', salary: 800, increase: false, id:6},
     ];
 
     return (
